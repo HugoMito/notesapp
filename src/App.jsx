b5ea2a5d7d667718fd98e8e5aea289a10f52b468
HugoMito <3hs.email@gmail.com>
2025-04-11 10:56:23 -0600

    Notes App Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,178 +1,185 @@
-import React, { useEffect, useState } from "react";
-import { MsalProvider } from "@azure/msal-react";
+import { useState, useEffect } from "react";
+import {
+  Authenticator,
+  Button,
+  Text,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Image,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { getUrl } from "aws-amplify/storage";
+import { uploadData } from "aws-amplify/storage";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-import Grid from '@mui/material/Grid';
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
 
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import logo3Hs from './assets/3Hs.png'
-import './App.css';
+export default function App() {
+  const [notes, setNotes] = useState([]);
 
-import { msalInstance } from "./main.jsx";
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [accessToken, setAccessToken] = useState(null);
-  const [applicationName, setApplicationName] = useState("N/A");
-  const [repoName, setRepoName] = useState("N/A");
-  const gitHubPAT = import.meta.env.VITE_GITHUB_PAT;
-  
   useEffect(() => {
-    const checkAccount = async () => {
-      try {
-        // Handle redirect responses
-        const redirectResponse = await msalInstance.handleRedirectPromise();
-        if (redirectResponse) {
-          handleLoginSuccess(redirectResponse);
-        } else {
-          const accounts = msalInstance.getAllAccounts();
-          if (accounts.length > 0) {
-            setUser(accounts[0]); // Set user
-            setIsLoggedIn(true); // User is logged in
-
-            // Attempt to acquire a token silently
-            await acquireAccessToken(accounts[0]);
-          }
-        }
-      } catch (error) {
-        console.error("Error handling redirect promise: ", error);
-      }
-    };
-
-    checkAccount(); // Call the function to check account status
+    fetchNotes();
   }, []);
 
-  const handleLoginSuccess = (authResult) => {
-    setUser(authResult.account);
-    setIsLoggedIn(true);
-    acquireAccessToken(authResult.account);
-  };
+  async function fetchNotes() {
+    const { data: notes } = await client.models.Note.list();
+    await Promise.all(
+      notes.map(async (note) => {
+        if (note.image) {
+          const linkToStorageFile = await getUrl({
+            path: ({ identityId }) => `media/${identityId}/${note.image}`,
+          });
+          console.log(linkToStorageFile.url);
+          note.image = linkToStorageFile.url;
+        }
+        return note;
+      })
+    );
+    console.log(notes);
+    setNotes(notes);
+  }
 
-  const acquireAccessToken = async (account) => {
-    const tokenRequest = {
-      scopes: ["openid", "profile", "User.Read", "Application.Read.All"],
-      account: account
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    console.log(form.get("image").name);
+
+    const { data: newNote } = await client.models.Note.create({
+      name: form.get("name"),
+      description: form.get("description"),
+      image: form.get("image").name,
+    });
+
+    console.log(newNote);
+    if (newNote.image)
+      if (newNote.image)
+        await uploadData({
+          path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
+
+          data: form.get("image"),
+        }).result;
+
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id }) {
+    const toBeDeletedNote = {
+      id: id,
     };
 
-    try {
-      const response = await msalInstance.acquireTokenSilent(tokenRequest);
-      setAccessToken(response.accessToken);
-    } catch (error) {
-      console.error("Token acquisition failed:", error);
-      if (error instanceof msal.InteractionRequiredAuthError) {
-        // If interaction is required, login again
-        msalInstance.loginRedirect(tokenRequest);
-      }
-    }
-  };
+    const { data: deletedNote } = await client.models.Note.delete(
+      toBeDeletedNote
+    );
+    console.log(deletedNote);
 
-  const login = () => {
-    const loginRequest = {
-      scopes: ["openid", "profile", "User.Read", "Application.Read.All"] // Specify your desired scopes
-    };
-    msalInstance.loginRedirect(loginRequest);
-  };
-
-  const getAzureApplications = async () => {
-    if (!accessToken) {
-      console.error("No access token available.");
-      return;
-    }
-
-    try {
-      const response = await fetch("https://graph.microsoft.com/v1.0/applications", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json"
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setApplicationName(data.value[0]?.displayName || "N/A"); // Set the first application's name
-    } catch (error) {
-      console.error("Error fetching applications:", error);
-    }
-  };
-
-  const getGitHubRepos = async () => {
-    if (!gitHubPAT) {
-      console.error("No GitHub PAT available.");
-      return;
-    }
-
-    try {
-      const response = await fetch("https://api.github.com/user/repos", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_PAT}`,
-          "Content-Type": "application/json"
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      // kktelas
-      const data = await response.json();
-      setRepoName(data.find(repo => repo.name === "notesapp")?.full_name || "N/A"); // Set the first repository's name
-    } catch (error) {
-      console.error("Error fetching GitHub repositories:", error);
-    }
+    fetchNotes();
   }
 
   return (
-    <MsalProvider instance={msalInstance}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.linkedin.com/in/3hsurl/" target="_blank">
-          <img src={logo3Hs} className="logo 3hs" alt="3Hs logo" />
-        </a>
-      </div>
-      <h2>Vite + React + 3Hs</h2>
+    <Authenticator>
+      {({ signOut }) => (
+        <Flex
+          className="App"
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          width="70%"
+          margin="0 auto"
+        >
+          <Heading level={1}>My Notes App</Heading>
+          <View as="form" margin="3rem 0" onSubmit={createNote}>
+            <Flex
+              direction="column"
+              justifyContent="center"
+              gap="2rem"
+              padding="2rem"
+            >
+              <TextField
+                name="name"
+                placeholder="Note Name"
+                label="Note Name"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <TextField
+                name="description"
+                placeholder="Note Description"
+                label="Note Description"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <View
+                name="image"
+                as="input"
+                type="file"
+                alignSelf={"end"}
+                accept="image/png, image/jpeg"
+              />
 
-      <div>
-        <h1>Azure OIDC Authentication</h1>
-        {isLoggedIn ? (
-          <Grid container spacing={1}>
-            <Grid size={{ xs: 12}} sx={{backgroundColor: "#3f51b5", color: "white", padding: 0, borderRadius: 4}}>
-              <h2>Welcome, {user.name}</h2>
-            </Grid>
-            <Grid size={{ xs: 12, sm: 3}} sx={{border: "1px solid #ccc", padding: 1, borderRadius: 4}}>
-              <button onClick={getAzureApplications}>Get First Application Name</button>
-              <p>{applicationName}</p>
-            </Grid>
-            <Grid size={{ xs: 12, sm: 3}} sx={{border: "1px solid #ccc", padding: 1, borderRadius: 4}}>
-              <button onClick={getGitHubRepos}>Get First GitHub Repos</button>
-              <p>{repoName}</p>
-            </Grid>
-          </Grid>          
-        ) : (
-          <div>
-            <button onClick={login}>Login with Microsoft Azure</button>
-          </div>
-        )}
-      </div>
-    </MsalProvider>
+              <Button type="submit" variation="primary">
+                Create Note
+              </Button>
+            </Flex>
+          </View>
+          <Divider />
+          <Heading level={2}>Current Notes</Heading>
+          <Grid
+            margin="3rem 0"
+            autoFlow="column"
+            justifyContent="center"
+            gap="2rem"
+            alignContent="center"
+          >
+            {notes.map((note) => (
+              <Flex
+                key={note.id || note.name}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                gap="2rem"
+                border="1px solid #ccc"
+                padding="2rem"
+                borderRadius="5%"
+                className="box"
+              >
+                <View>
+                  <Heading level="3">{note.name}</Heading>
+                </View>
+                <Text fontStyle="italic">{note.description}</Text>
+                {note.image && (
+                  <Image
+                    src={note.image}
+                    alt={`visual aid for ${notes.name}`}
+                    style={{ width: 400 }}
+                  />
+                )}
+                <Button
+                  variation="destructive"
+                  onClick={() => deleteNote(note)}
+                >
+                  Delete note
+                </Button>
+              </Flex>
+            ))}
+          </Grid>
+          <Button onClick={signOut}>Sign Out</Button>
+        </Flex>
+      )}
+    </Authenticator>
   );
 }
-
-          // <div>
-          //   <h2>Welcome, {user.name}</h2>
-          //   <button onClick={getAzureApplications}>Get First Application Name</button>
-          //   <p>Application name: {applicationName}</p>
-          //   <button onClick={getGitHubRepos}>Get GitHub Repos</button>
-          //   <p>GitHub Repo Full Name: {repoName}</p>
-          // </div>
-export default App;
