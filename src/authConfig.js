1af66d57fad2d0ad43e08017d5ce1430a3a3475a
HugoMito <3hs.email@gmail.com>
2025-04-10 19:35:16 -0600

    Adding redirect URI

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,12 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 export const msalConfig = {
     auth: {
         clientId: "55eb0c24-1dd0-457d-a9ca-756fdc5e78f4",
         authority: "https://login.microsoftonline.com/799c6fdd-221a-48a3-bd58-d740bf89ba38",
-        redirectUri: "http://localhost:5173"
+        redirectUri: isLocalhost
+            ? "http://localhost:5173"
+            : "https://main.d2kfwzbpqm9rnf.amplifyapp.com/",
     },
     cache: {
         cacheLocation: "sessionStorage",
